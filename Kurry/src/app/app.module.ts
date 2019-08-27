d90d9a7f05d2265186d7dd56a7a50903520c5e97
Duplicity <abraham.easley3@gmail.com>
2019-08-27 01:15:03 -0400

    Co-authored-by: Sfrjoshpro <sfrjoshpro@gmail.com>

--- Kurry/src/app/app.module.ts
+++ Kurry/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SideComponent } from './component/side/side.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ContentComponent } from './component/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
